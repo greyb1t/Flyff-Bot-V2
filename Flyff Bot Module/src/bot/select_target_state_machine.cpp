622d3b8917a03e1fd8a4b248990e7fa5bb84fe23
greyb1t <gresan6@hotmail.com>
2020-05-08 20:04:15 +0200

    Bypassed eclipse flyff protection against bots

--- Flyff Bot Module/src/bot/select_target_state_machine.cpp
+++ Flyff Bot Module/src/bot/select_target_state_machine.cpp
@@ -7,6 +7,17 @@ namespace bot {
 
 StateStatusReturnValue SelectTargetStateMachine::Select(
     const Entity& entity ) {
+  /*
+    Eclipse Flyff added protection against bots by checking if the 
+    cursor is on a monster when the user selects it.
+
+    That ruined the bot because I reset the cursor pos IMMEDIATELY 
+    after I have clicked on the monster. 
+    Therefore, the game registers it as I never had my mouse on the monster.
+
+    Simply fixed by never reseting the cursor positin after having selected an entity.
+  */
+
   const auto target_selection_state = GetCurrentState<TargetSelectionStates>();
 
   const auto botcore = bot_->GetBotCore();
@@ -51,14 +62,14 @@ StateStatusReturnValue SelectTargetStateMachine::Select(
 
           // Set both the simulated and real cursor pos, because simulated
           // cursor pos is not enough.
-          botcore->SetSimulatedCursorPos( old_cursor_pos_ );
+          //botcore->SetSimulatedCursorPos( old_cursor_pos_ );
 
           // If the cursor is inside the flyff window, then set the real cursor
           // pos to the previous one If we always do it, the cursor lags when
           // doing other stuff
           if ( WindowFromPoint( old_cursor_pos_ ) ==
                botcore->GetTargetWindow() ) {
-            SetCursorPos( old_cursor_pos_.x, old_cursor_pos_.y );
+            //SetCursorPos( old_cursor_pos_.x, old_cursor_pos_.y );
           }
 
           SetNextState( TargetSelectionStates::ResetCursor );
@@ -73,8 +84,8 @@ StateStatusReturnValue SelectTargetStateMachine::Select(
 
     case TargetSelectionStates::ResetCursor: {
       // Prevent the bot from locking the cursor
-      POINT invalid_screen_pos{ -1, -1 };
-      botcore->SetSimulatedCursorPos( invalid_screen_pos );
+      //POINT invalid_screen_pos{ -1, -1 };
+      //botcore->SetSimulatedCursorPos( invalid_screen_pos );
 
       // Reset the TargetSelectionState for next entity
       SetNextState( TargetSelectionStates::HoverCursor );
